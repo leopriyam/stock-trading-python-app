9c9e3cef2373f36f2ffa917147b9e51d7edc2ec8
Priyam Ganguly <priyamganguly0@gmail.com>
2025-10-17 23:00:20 -0400

    Add files via upload

--- pycache/scheduler.py
+++ pycache/scheduler.py
@@ -1,1 +1,17 @@
+import schedule
+import time
+from datetime import datetime
+from script import run_stock_job
 
+def basic_job():
+    print("Job started at ", datetime.now())
+
+#run every minute
+schedule.every().minute.do(basic_job)
+
+schedule.every().day.at("09:00").do(run_stock_job)
+schedule.every().minute.do(run_stock_job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
